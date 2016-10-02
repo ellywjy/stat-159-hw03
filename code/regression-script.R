a48b3579f3f193e58da9e4032d6b32dca260f57a
Elly Wang <ellywjy@berkeley.edu>
2016-10-02 14:27:57 -0700

    Fixed regression-script.R, generated RData file and scatterplot png and pdf

--- code/regression-script.R
+++ code/regression-script.R
@@ -1,13 +1,14 @@
 library(ggplot2)
-ad_data <- read.csv("../data/Advertising.csv")
+ad_data <- read.csv("data/Advertising.csv")
 
 regression <- lm(Sales~TV, ad_data)
 summary(regression)
 
 ggplot(ad_data, aes(TV, Sales)) + geom_point() + geom_smooth(method=lm)
-ggsave("../images/scatterplot-tv-sales.png")
+ggsave("images/scatterplot-tv-sales.png")
+ggsave("images/scatterplot-tv-sales.pdf")
 
-save(regression, "regression.RData")
+save(regression, file = "data/regression.RData")
 
 
 
