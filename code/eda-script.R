8e0fdf4bcfd00cd992dc387150191bde287e374b
Elly Wang <ellywjy@berkeley.edu>
2016-10-02 14:21:09 -0700

    Fixed sink in eda-script, ran make and generated eda-output and png histograms

--- code/eda-script.R
+++ code/eda-script.R
@@ -2,8 +2,9 @@
 library(ggplot2)
 ad_data <- read.csv("data/Advertising.csv")
 
-ad_summary <- summary(ad_data)
-sink("data/eda-output.txt", type = ad_summary)
+sink("data/eda-output.txt")
+summary(ad_data)
+sink()
 
 ggplot(ad_data) + geom_histogram(aes(TV))
 ggsave("images/histogram-tv.png")
