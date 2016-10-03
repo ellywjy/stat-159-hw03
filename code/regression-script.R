1f2da2477b975b769034a6ec033eb17a1e69da2b
Elly Wang <ellywjy@berkeley.edu>
2016-10-03 10:44:17 -0700

    Saved summary in RData file

--- code/regression-script.R
+++ code/regression-script.R
@@ -2,13 +2,13 @@ library(ggplot2)
 ad_data <- read.csv("data/Advertising.csv")
 
 regression <- lm(Sales~TV, ad_data)
-summary(regression)
+reg_summary <- summary(regression)
 
 ggplot(ad_data, aes(TV, Sales)) + geom_point() + geom_smooth(method=lm)
 ggsave("images/scatterplot-tv-sales.png")
 ggsave("images/scatterplot-tv-sales.pdf")
 
-save(regression, file = "data/regression.RData")
+save(regression, reg_summary, file = "data/regression.RData")
 
 
 
