70016bc633dd997d3ff54c6aeda52b3a557d428c
Elly Wang <ellywjy@berkeley.edu>
2016-10-05 13:30:04 -0700

    Modified scatterplot

--- code/regression-script.R
+++ code/regression-script.R
@@ -4,9 +4,10 @@ ad_data <- read.csv("data/Advertising.csv")
 regression <- lm(Sales~TV, ad_data)
 reg_summary <- summary(regression)
 
-ggplot(ad_data, aes(TV, Sales)) + geom_point(aes(color = 'coral'), show.legend = FALSE) + 
+ggplot(ad_data, aes(TV, Sales)) + geom_point(aes(color = 'coral'), show.legend = FALSE, size = 0.75) + 
   geom_smooth(method=lm, se = FALSE) +
-  labs(title = 'Sales vs. TV', x = "TV (thousands of dollars)", y = "Sales (thousands of units)")
+  labs(title = 'Sales vs. TV', x = "TV (thousands of dollars)", y = "Sales (thousands of units)") + 
+  theme_bw()
 
 ggsave("images/scatterplot-tv-sales.png")
 ggsave("images/scatterplot-tv-sales.pdf")
