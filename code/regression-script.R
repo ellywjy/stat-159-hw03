7e9ce1ed67abf67461881899f3b9f50d5de46ace
Elly Wang <ellywjy@berkeley.edu>
2016-10-05 13:26:45 -0700

    Added more features to scatterplot

--- code/regression-script.R
+++ code/regression-script.R
@@ -4,7 +4,10 @@ ad_data <- read.csv("data/Advertising.csv")
 regression <- lm(Sales~TV, ad_data)
 reg_summary <- summary(regression)
 
-ggplot(ad_data, aes(TV, Sales)) + geom_point() + geom_smooth(method=lm)
+ggplot(ad_data, aes(TV, Sales)) + geom_point(aes(color = 'coral'), show.legend = FALSE) + 
+  geom_smooth(method=lm, se = FALSE) +
+  labs(title = 'Sales vs. TV', x = "TV (thousands of dollars)", y = "Sales (thousands of units)")
+
 ggsave("images/scatterplot-tv-sales.png")
 ggsave("images/scatterplot-tv-sales.pdf")
 
