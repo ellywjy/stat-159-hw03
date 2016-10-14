dc368c0f5ce212a2292492f934dd5a6473f12a6c
Elly Wang <ellywjy@berkeley.edu>
2016-10-14 12:29:47 -0700

    Changed CorrMatrix to Upper Tri

--- code/scripts/eda-script.R
+++ code/scripts/eda-script.R
@@ -2,6 +2,8 @@ library(ggplot2)
 
 ad_data <- read.csv("data/Advertising.csv")
 corr_matrix <- cor(ad_data[-1])
+lower <- corr_matrix
+lower[lower.tri(corr_matrix, diag =FALSE)] <- ""
 
 sink("data/eda-output.txt")
 cat("Summary Statistics for TV:\n")
@@ -13,7 +15,7 @@ summary(ad_data$Newspaper)
 cat("Summary Statistics for Sales:\n")
 summary(ad_data$Sales)
 cat("\n Correlation Matrix:\n")
-cor(ad_data[-1])
+lower
 sink()
 
 ggplot(ad_data) + geom_histogram(aes(TV))
