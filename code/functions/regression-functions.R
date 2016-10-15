03fb2750561aed674cd2326f5a702e27c479940b
Elly Wang <ellywjy@berkeley.edu>
2016-10-14 17:18:31 -0700

    Added descriptions to functions

--- code/functions/regression-functions.R
+++ code/functions/regression-functions.R
@@ -1,19 +1,31 @@
+#Description: computes the residuals squares given a regression model
+#Expected Input: a regression model produced using lm()
+#Expected Output: Sum of residuals squared, a double
 residual_sum_squares <- function(x){
 	sum(x$residuals^2)
 }
 
+#Description: computes the total sum squared given a regression model
+#Expected Input: a regression model produced using lm()
+#Expected Output: Total sum squared: Sum of (Y-Ybar)^2, a double
 total_sum_squares <- function(x){
 
 	sum(((x$model[[1]])-mean(x$model[[1]]))^2)
 
 }
 
+#Description: computes R^2 given a regression model
+#Expected Input: a regression model produced using lm()
+#Expected Output: R squared value for the regression model
 r_squared <- function(x){
 
 	1-(residual_sum_squares(x)/total_sum_squares(x))
 
 }
 
+#Description: computes F-statistics given a regression model
+#Expected Input: a regression model produced using lm()
+#Expected Output: F-statistic of the regression model, a double
 f_statistic <- function(x){
 
 	TSS <- total_sum_squares(x)
@@ -25,6 +37,9 @@ f_statistic <- function(x){
 	f
 }
 
+#Description: computes residual standard error given a regression model
+#Expected Input: a regression model produced using lm()
+#Expected Output: residuals standard error, a double
 residual_std_error <- function(x){
 
 	RSS <- residual_sum_squares(x)
